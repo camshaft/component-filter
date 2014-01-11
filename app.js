b7b9692b8f64504a914d7b00503e63530cfca0f8
CamShaft <bytheway.cameron@gmail.com>
2014-01-11 06:22:22 -0700

    fix app and vendor filtering

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-var package = require(process.cwd() + '/component.json');
-var remove = require('.').remove;
+var pkg = require(process.cwd() + '/component.json');
+var remove = require('./').remove;
 
 module.exports = function(builder) {
   builder.hook('before styles', function(pkg) {
-    if (pkg.config.repo !== package.config.repo) return;
-    remove(pkg);  
+    if (!pkg.root) return;
+    remove(pkg);
   });
   builder.hook('before scripts', function(pkg) {
-    if (pkg.config.repo !== package.config.repo) return;
-    remove(pkg);  
+    if (!pkg.root) return;
+    remove(pkg);
   });
 };
--- vendor.js
+++ vendor.js
@@ -1,13 +1,13 @@
-var package = require(process.cwd() + '/component.json');
-var remove = require('.').remove;
+var pkg = require(process.cwd() + '/component.json');
+var remove = require('./').remove;
 
 module.exports = function(builder) {
   builder.hook('before styles', function(pkg) {
-    if (pkg.config.repo === package.config.repo) return pkg.config.dependencies;
-    remove(pkg);  
+    if (pkg.root) return pkg.config.dependencies = {};
+    remove(pkg);
   });
   builder.hook('before scripts', function(pkg) {
-    if (pkg.config.repo === package.config.repo) return pkg.config.dependencies;
-    remove(pkg);  
+    if (pkg.root) return pkg.config.dependencies = {};
+    remove(pkg);
   });
 };
